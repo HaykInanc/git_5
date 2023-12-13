694defd15a16e8b1cd9aed255d76e21df294f8be
HaykInanc <hayk.inanc@gmail.com>
2023-12-13 23:06:14 +0400

    add Person class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = new Person("Анатой", "Ушанов");
+        System.out.println(person.info());
     }
 }
+
+//1) создать класс Person
+//2) добавить приватные свойства firstName, lastName
+//3) добавить конструктор, который получает эти значения через параметры и присваивает их свойствам
+//4) добавить метод info который возвращает имя и фамилию персоны через пробел
+//4.1) в main создать персону и проверить ее метод info
+//5) добавить коммит "add Person class"
+//6) залить в github
